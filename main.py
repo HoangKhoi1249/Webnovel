a8f56db864644480d0b7f537639ab8250a4966ae
Koiy <hihe1249@gmail.com>
2025-11-01 08:49:32 +0700

    update option to analyze volumes

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 import json
 import translate
 
-def main():
+def main(split_volume=True):
     vol_lists, chapters_list = cp.collect_files()
     if vol_lists:
         print("Volumes found:", vol_lists)
     for chapter in chapters_list:
-        if util.is_2d_list(chapters_list):
+        if util.is_2d_list(chapters_list) and split_volume:
             for chap_path in chapter:
                 print(chap_path)
         else:
