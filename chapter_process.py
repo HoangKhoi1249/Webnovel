7902146e66fc0d3cae81047cef2be320f950037e
Koiy <nghiemdinhkhoinguyen@gmail.com>
2025-11-04 21:23:09 +0700

    Fix translation logic in full_translate function to correctly call the translation method

--- chapter_process.py
+++ chapter_process.py
@@ -123,7 +123,7 @@ def full_translate(path):
     is_success = False
     while not is_success:
         try:
-            translated_content =  content#tl.translate_content(content)
+            translated_content =  tl.translate_content(content)
             save_content(path, translated_content)
             print("Dịch hoàn tất! đang bắt đầu chương tiếp theo...")
             is_success = True
